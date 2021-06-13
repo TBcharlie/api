fbd798d7686772da0324226c78af9925939842ae
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2021-06-13 18:40:31 -0300

    update route recebe email e senha

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const ControllerUsuario = require('./controllers/ControllerUsuario');
 router.get('/usuarios', ControllerUsuario.buscarTodosUsuarios);//buscar todos usuarios
-router.get('/usuario/:email', ControllerUsuario.buscarUmUsuario);//buscar usuario pelo email
+router.get('/usuario/:email/:senha', ControllerUsuario.buscarUmUsuario);//buscar usuario pelo email
 router.post('/usuario', ControllerUsuario.inserirUsuario);//inserir usuario na base de dados
 router.put('/usuario/:id', ControllerUsuario.alterarUsuario);//alterar dados do usuario
 
