f7d8a32efbace780648a306f62b7640122cfd5cf
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2021-06-13 22:29:12 -0300

    update service pedido insere usando parametro body

--- src/services/ServicePedido.js
+++ src/services/ServicePedido.js
@@ -5,7 +5,7 @@ module.exports = {
     inserirPedido: (comprador, email, compra, endereco)=> {
         return new Promise((aceito, rejeitado)=> {
     
-            db.query('INSERT INTO pedidos (comprador, email, compra, endereco) VALUES (?, ?, ?, ?)',
+            db.body('INSERT INTO pedidos (comprador, email, compra, endereco) VALUES (?, ?, ?, ?)',
                 [comprador, email, compra, endereco],
                 (error, results)=>{
                     if(error){ rejeitado(error); return; }
